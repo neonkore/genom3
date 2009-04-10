61f51a807dba5497de3e31e2f41689846feabbb2
root <root@debian.(none)>
2009-04-10 14:15:19 +0200

    Added AST classes with basic debug output

---
+++ parser/ast.cpp
@@ -0,0 +1,130 @@
+/* 
+ * Copyright (c) 2009 LAAS/CNRS                      
+ * All rights reserved.
+ *
+ * Redistribution and use  in source  and binary  forms,  with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *   1. Redistributions of  source  code must retain the  above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice,  this list of  conditions and the following disclaimer in
+ *      the  documentation  and/or  other   materials provided  with  the
+ *      distribution.
+ *
+ * THIS  SOFTWARE IS PROVIDED BY  THE  COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND  ANY  EXPRESS OR IMPLIED  WARRANTIES,  INCLUDING,  BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES  OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR  PURPOSE ARE DISCLAIMED. IN  NO EVENT SHALL THE COPYRIGHT
+ * HOLDERS OR      CONTRIBUTORS  BE LIABLE FOR   ANY    DIRECT, INDIRECT,
+ * INCIDENTAL,  SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF  SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE   OF THIS SOFTWARE, EVEN   IF ADVISED OF   THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+#include "ast.h"
+
+#include <iostream>
+
+using namespace std;
+using namespace G3nom;
+
+Component::Component() 
+{}
+
+Component::~Component() 
+{
+    for(Service::Map::iterator it = services.begin(); it != services.end(); ++it)
+	delete it->second;
+    for(Task::Map::iterator it2 = tasks.begin(); it2 != tasks.end(); ++it2)
+	delete it2->second;
+    for(Port::Map::iterator it3 = ports.begin(); it3 != ports.end(); ++it3)
+	delete it3->second;
+}
+
+
+void Component::debug()
+{
+    cout << "Component: " << name << " v. " << version << endl;
+    cout << "\tLanguage (plugin) : " << pluginLanguage << endl;
+    cout << "\tIDS Struct Name: " << IDSStructName << endl;
+
+    cout << endl << "Services: " << services.size() << endl;
+    Service::Map::const_iterator it;
+    for(it = services.begin(); it != services.end(); ++it) {
+	cout << "\t" << it->first << " : " << endl;
+	it->second->debug();
+	cout << endl;
+    }
+
+    cout << endl << "Tasks: " << tasks.size() << endl;
+    Task::Map::const_iterator it;
+    for(it = tasks.begin(); it != tasks.end(); ++it) {
+	cout << "\t" << it->first << " : " << endl;
+	it->second->debug();
+	cout << endl;
+    }
+
+    cout << endl << "Ports: " << ports.size() << endl;
+    Port::Map::const_iterator it;
+    for(it = ports.begin(); it != ports.end(); ++it) {
+	cout << "\t* " << it->first << " : " << endl;
+	it->second->debug();
+	cout << endl;
+    }
+}
+
+void Component::addTask(const std::string &name, Task* task)
+{
+    tasks.insert(name, task);
+}
+
+void Component::addService(const std::string &name, Service* s)
+{
+    services.insert(name, s);
+}
+
+void Component::addPort(const std::string &name, Port *port)
+{
+    ports.insert(name,port);
+}
+
+void Port::debug()
+{
+    if(type == Incoming)
+      cout << "Inport: ";
+    else
+      cout << "Outport: ";
+
+    cout << name << " of type " << /*idlType->debug() <<*/ endl;
+}
+
+void Task::debug()
+{
+    cout << "priority: " << priority << ", stackSize: " << stackSize << ", period: " << period << endl;
+    cout << "Codels:" << endl;
+    Codel::Map::const_iterator it;
+    for(it = codels.begin(); it != codels.end(); ++it) {
+	cout << "\t-- " << it->first << " : " << endl;
+	it->second->debug();
+	cout << endl;
+    }    
+}
+
+void Service::debug()
+{
+    cout << "doc: " << doc << endl;
+    cout << "Parent task: " << taskName << endl;
+    cout << "Codels:" << endl;
+    Codel::Map::const_iterator it;
+    for(it = codels.begin(); it != codels.end(); ++it) {
+	cout << "\t-- " << it->first << " : " << endl;
+	it->second->debug();
+	cout << endl;
+    }    
+}
+
---
+++ parser/ast.h
@@ -0,0 +1,131 @@
+/* 
+ * Copyright (c) 2009 LAAS/CNRS                      
+ * All rights reserved.
+ *
+ * Redistribution and use  in source  and binary  forms,  with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *   1. Redistributions of  source  code must retain the  above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice,  this list of  conditions and the following disclaimer in
+ *      the  documentation  and/or  other   materials provided  with  the
+ *      distribution.
+ *
+ * THIS  SOFTWARE IS PROVIDED BY  THE  COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND  ANY  EXPRESS OR IMPLIED  WARRANTIES,  INCLUDING,  BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES  OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR  PURPOSE ARE DISCLAIMED. IN  NO EVENT SHALL THE COPYRIGHT
+ * HOLDERS OR      CONTRIBUTORS  BE LIABLE FOR   ANY    DIRECT, INDIRECT,
+ * INCIDENTAL,  SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF  SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE   OF THIS SOFTWARE, EVEN   IF ADVISED OF   THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+#ifndef G3NOM_AST_H
+#define G3NOM_AST_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+namespace G3nom {
+
+class IDLType;
+
+class Port {
+  public:
+    enum Type { Incoming, Outgoing};
+    typedef std::map<std::string, Port*> Map;
+
+    Port() {}
+    Port(std::string name, IDLType type, bool incoming)
+      : name(name), type(type), type(incoming ? Incoming: Outgoing)
+    {}
+    
+    std::string name;
+    Type type;
+    IDLType idlType;
+};
+
+class Codel {
+  public:
+    typedef std::map<std::string, Codel*> Map;
+
+    Codel() {}
+
+    void debug();
+
+    std::string name;
+    std::vector<std::string> inTypes;
+    std::vector<std::string> outTypes;
+    std::vector<std::string> inPorts;
+    std::vector<std::string> outPorts;
+};
+
+class Task {
+  public:
+    typedef std::map<std::string, Task*> Map;
+
+    Task() {}
+
+    void debug();
+
+    int priority;
+    int period; // in ms
+    int stackSize; // in kbytes
+
+  private:
+    Codel::Map codels; 
+    //std::string connectedPort;
+};
+
+class Service {
+  public:
+    typedef std::map<std::string, Service*> Map;
+    enum Type { Control, Exec, Init };
+
+    Service() {}
+
+    void debug();
+
+    Type type;
+    std::string doc;
+    std::string taskName;
+
+  private:
+    Codel::Map codels;
+    std::vector<std::string> incompatibleServices;
+};
+
+class Component {
+  public:
+    Component();
+    ~Component();
+
+    void debug();
+
+    void addTask(const std::string& name, G3nom::Task* task);
+    void addService(const std::string& name, G3nom::Service* task);
+    void addPort(const std::string& name, G3nom::Port* port);
+
+    std::string name;
+    std::string pluginLanguage;
+    std::string version;
+    std::string IDSStructName;
+
+  private:
+    /// @todo use smart pointers or smart maps
+    Task::Map tasks;
+    Service::Map services;
+    Port::Map ports;
+};
+
+}
+
+#endif // G3NOM_AST_H

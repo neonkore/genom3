b4c076da985bfb3d367d28b0a121d4e6a6ec8826
Cedric <ccpasteur@debian.(none)>
2009-04-15 17:16:57 +0200

    Missing file

---
+++ utils/idltype.h
@@ -0,0 +1,56 @@
+/* 
+ * Copyright (c) 2009 LAAS/CNRS                      
+ * All rights reserved.
+ *
+ * Redistribution and use  in source  and binary  forms,  with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *   1. Redistributions of  source  code must retain the  above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice,  this list of  conditions and the following disclaimer in
+ *      the  documentation  and/or  other   materials provided  with  the
+ *      distribution.
+ *
+ * THIS  SOFTWARE IS PROVIDED BY  THE  COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND  ANY  EXPRESS OR IMPLIED  WARRANTIES,  INCLUDING,  BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES  OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR  PURPOSE ARE DISCLAIMED. IN  NO EVENT SHALL THE COPYRIGHT
+ * HOLDERS OR      CONTRIBUTORS  BE LIABLE FOR   ANY    DIRECT, INDIRECT,
+ * INCIDENTAL,  SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF  SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE   OF THIS SOFTWARE, EVEN   IF ADVISED OF   THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+#ifndef G3NOM_IDLTYPE_H
+#define G3NOM_IDLTYPE_H
+
+#include <memory>
+
+class IDLType {
+  public:
+    enum Kind { Unknown, Predefined, Struct, Sequence, Native };
+    /// @todo find a better way to hanfdle types (shared pointers with ref counting ?)
+    typedef std::auto_ptr<IDLType> Ptr;
+
+    IDLType() {}
+    ~IDLType() {}
+
+  private:
+    Kind m_kind;
+};
+
+class SequenceType : public IDLType {
+  public:
+    SequenceType() {}
+
+  private:
+    IDLType::Ptr m_memberType;
+};
+
+#endif // G3NOM_IDLTYPE_H

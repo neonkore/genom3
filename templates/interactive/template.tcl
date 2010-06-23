b80a43de78cf2602299b5df4acbd113e0001f174
Anthony Mallet <anthony.mallet@laas.fr>
2010-06-23 11:57:54 +0200

    Implement new -b option in 'interactive' template, for batch mode.
    It might be useful to pipe commands to the 'genom3 interactive' template. The
    -b (batch) option disable line editing facility and other fancy output in the
    template for this use case. The template will also fail on error (unlike in
    true interactive mode, where a new prompt is displayed).

--- templates/interactive/template.tcl
+++ templates/interactive/template.tcl
@@ -32,46 +32,54 @@ template usage {*}{
     "for development of new templates or troubleshooting existing ones.\n"
     "\n"
     "Supported options:\n"
+    "  -b\t\t\tbatch mode: disable line editing facility\n"
     "  -h.--help\t\tprint usage summary (this text)"
 }
 
 # defaults
+variable tcl_interactive 1
 engine mode -overwrite verbose
 
 template options {
+    -b			{ variable tcl_interactive 0 }
     -h - --help		{ engine mode +verbose; puts [template usage]; exit 0 }
 }
 
 # provide a default 'interactive' function in case eltclsh cannot be loaded
 proc interactive {} {
+    global tcl_interactive
+
     while {1} {
-	puts -nonewline {% }; flush stdout
+	if {$tcl_interactive} { puts -nonewline {% }; flush stdout }
 	while {[gets stdin line] >= 0} {
 	    append cmd $line
 	    if {![info complete $cmd]} {
 		append cmd "\n"
-		puts -nonewline {> }; flush stdout
+		if {$tcl_interactive} { puts -nonewline {> }; flush stdout }
 		continue
 	    }
 
-	    catch {uplevel #0 $cmd} cmd
+	    set r [catch {uplevel #0 $cmd} cmd opts]
+	    if {!$tcl_interactive && $r} { return -options $opts $cmd }
 	    if {$cmd != ""} { puts $cmd }
 	    unset cmd
-	    puts -nonewline {% }; flush stdout
+	    if {$tcl_interactive} { puts -nonewline {% }; flush stdout }
 	}
 	if {![info exists cmd]} break
 	unset cmd
-	puts {}
+	if {$tcl_interactive} { puts {} }
     }
     return
 }
 
-# load eltclsh
-catch { package require eltclsh }
+if {$tcl_interactive} {
+    # load eltclsh
+    catch { package require eltclsh }
 
-# interactive loop
-puts "
+    # interactive loop
+    puts "
 Interactive Tcl template - [dotgen genom version]
 Type Ctrl-d or 'exit' to leave."
+}
 
 interactive

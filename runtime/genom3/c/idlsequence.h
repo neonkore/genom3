9be9b7ebccdf0a55f691087d88ddbb19fe056d2b
Anthony Mallet <anthony.mallet@laas.fr>
2011-03-18 18:00:56 +0100

    Add extra consistency check in genom3_sequence_resize() C helper macro
    When resizing a sequence, any _length > 0 should be sufficient to ensure that
    _buffer is not NULL. However, adding an extra check does not hurt and since
    this macro is public, this may help.

--- runtime/genom3/c/idlsequence.h
+++ runtime/genom3/c/idlsequence.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 LAAS/CNRS
+ * Copyright (c) 2010-2011 LAAS/CNRS
  * All rights reserved.
  *
  * Redistribution and use  in source  and binary  forms,  with or without
@@ -59,7 +59,7 @@ _genom3_resize_buffer(uint32_t length, size_t esize,
 
   *_maximum = length;
   if (length < *_length) *_length = length;
-  if (*_length > 0) memcpy(buffer, *_buffer, *_length * esize);
+  if (*_length > 0 && *_buffer) memcpy(buffer, *_buffer, *_length * esize);
   if (*_release) (*_release)(*_buffer);
   *_buffer = buffer;
   *_release = free;

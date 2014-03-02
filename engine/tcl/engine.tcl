755c56c76dfd9993598a6368f2220772a6b26266
Anthony Mallet <anthony.mallet@laas.fr>
2014-03-02 14:34:53 +0100

    Fix one incorrect list vs. string usage in the tcl engine
    The tcl 'interp alias' command expects a list of arguments: one must add a {*}
    to the result of the [namespace code] call so that it expands properly.
    
    It appears that tcl<8.6 was doing an extra (incorrect) leval of expansion,
    thereby hiding this problem, but the bug fix still works for tcl<8.6.

--- engine/tcl/engine.tcl
+++ engine/tcl/engine.tcl
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2010-2013 LAAS/CNRS
+# Copyright (c) 2010-2014 LAAS/CNRS
 # All rights reserved.
 #
 # Redistribution  and  use  in  source  and binary  forms,  with  or  without
@@ -508,7 +508,7 @@ namespace eval engine {
       close $tfile
 
       # execute template program in slave interpreter
-      slave alias puts [namespace code "slave-output $out"]
+      slave alias puts {*}[namespace code "slave-output $out"]
       slave eval {
         trace add execution gsource leavestep {slave-monitor [eline]}
       }

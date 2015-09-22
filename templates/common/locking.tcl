30c5f43b3e918334017862b9bd26d4a9d8ffa413
Anthony Mallet <anthony.mallet@laas.fr>
2015-09-22 18:30:36 +0200

    Add a template helper function computing if a codel is thread-safe
    This TCL function is not used by genom but provided as a convenience function
    for templates.
    
    A codel is considered to be thread-safe if its parameter list does not conflict
    with codels running in other tasks. The function considers that there is a
    parameter conflict if the codel's input are found in other codels outputs, or
    if the codel's output are found in other codels inputs.
    The "start" state of permanent activities of tasks are explicitely not
    considered, because those are run before any other codel can run.
    
    The analysis is purely static, using the .gen description.
    All reachable states are considered, but incompatible activities are accounted
    for, as they are not supposed to run concurrently.

---
+++ templates/common/locking.tcl
@@ -0,0 +1,152 @@
+#
+# Copyright (c) 2015 LAAS/CNRS
+# All rights reserved.
+#
+# Permission to use, copy, modify, and distribute this software for any purpose
+# with or without   fee is hereby granted, provided   that the above  copyright
+# notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS  SOFTWARE INCLUDING ALL  IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER RESULTING  FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
+# PERFORMANCE OF THIS SOFTWARE.
+#
+#                                             Anthony Mallet on Tue Sep 22 2015
+#
+
+# --- thread-safe-codel ----------------------------------------------------
+#
+# Return an empty list if a codel can safely execute in parallel with other
+# codels of other tasks. This is a static analysis of all the FSMs, so it's
+# suboptimal.
+#
+# If the codel is not thread safe, the function returns the list of conflicting
+# codels.
+#
+proc thread-safe-codel { component codel } {
+  set conflicts [list]
+
+  # codel info
+  set input [list]
+  foreach p [$codel parameters in inout] {
+    if {[$p src] ne "local"} { lappend input $p }
+  }
+  set output [list]
+  foreach p [$codel parameters out inout] {
+    if {[$p src] ne "local"} { lappend output $p }
+  }
+
+  if {[catch {$codel task} task]} { set task "" }
+
+  # scan permanent activities in all other tasks
+  foreach t [$component tasks] {
+    if {$t == $task} continue
+
+    # do not consider the start state which is guaranteed to have run already
+    set start [$t fsm ::[$component name]::start]
+    if {[llength $start]} {
+      foreach c [codel-reachable $t [$start yield]] {
+        lappend conflicts {*}[codel-instersect $input $output $c]
+      }
+    }
+  }
+
+  # scan all services in other tasks
+  foreach s [$component services] {
+    if {[catch {$s task} t]} { set t "" }
+    if {$t == $task} continue
+
+    # validation codels and regular codels
+    foreach c [$s validate] {
+      lappend conflicts {*}[codel-instersect $input $output $c]
+    }
+
+    # do not further consider this service if it interrupts the service to
+    # which the given codel belongs
+    if {![catch {$codel service} service]} {
+      if {$service in [$s interrupts]} continue
+    }
+
+    # regular codels
+    foreach c [list {*}[$s validate] {*}[$s codels]] {
+      lappend conflicts {*}[codel-instersect $input $output $c]
+    }
+  }
+
+  return $conflicts
+}
+
+
+# --- thread-safe-error ----------------------------------------------------
+#
+# Log an error message with the conflicts
+#
+proc thread-safe-error { codel conflicts } {
+  set file [lindex [split [lindex [$codel loc] 0] /] end]
+  set prefix "$file:[lindex [$codel loc] 1]"
+  append prefix " async codel [$codel name] is incompatible with"
+
+  foreach c $conflicts {
+    set file2 [lindex [split [lindex [$c loc] 0] /] end]
+    set file2 "$file2:[lindex [$c loc] 1]"
+
+    puts stderr "$prefix [$c name]"
+    puts stderr "$file2: [$c name] defined here"
+  }
+  return -code error "aborting"
+}
+
+
+# --- internal helper functions --------------------------------------------
+
+proc codel-reachable { obj states {codels {}} } {
+  foreach e $states {
+    set c [$obj fsm $e]
+    if {$c eq ""} continue
+    if {$c in $codels} continue
+
+    lappend codels $c
+    lappend codels {*}[codel-reachable $obj [$c yields] $codels]
+  }
+
+  return $codels
+}
+
+proc codel-instersect { input output codel } {
+  set input2 [list]
+  foreach p [$codel parameters in inout] {
+    if {[$p src] ne "local"} { lappend input2 $p }
+  }
+  set output2 [list]
+  foreach p [$codel parameters out inout] {
+    if {[$p src] ne "local"} { lappend output2 $p }
+  }
+
+  foreach p $input {
+    foreach q $output2 {
+      if {[parameter-match $p $q]} { return $codel }
+    }
+  }
+  foreach p $output {
+    foreach q $input2 {
+      if {[parameter-match $p $q]} { return $codel }
+    }
+  }
+
+  return [list]
+}
+
+proc parameter-match { p q } {
+  if {[$p base] ne [$q base]} { return no }
+  set pm [$p member]
+  set qm [$q member]
+
+  if {$pm eq ""} { return yes }
+  if {$qm eq ""} { return yes }
+  if {[string first $pm $qm] == 0} { return yes }
+  if {[string first $qm $pm] == 0} { return yes }
+  return no
+}

neonkore/genom3

commit cd37430cd6ec8ff7a61f8b0d95783abd7b7a33e5

Make sure to write template files in utf-8

engine/tcl/engine.tcl
@@ -271,9 +271,11 @@ namespace eval engine {
 		return [chan create write [namespace code [list strchan $dst]]]
 	    }
 	    file|read {
-		set dst [file join [dotgen template dir] $dst]
-		template message "reading $dst"
-		return [::open $dst r]
+              set dst [file join [dotgen template dir] $dst]
+              message "reading $dst"
+              set c [::open $dst r]
+              fconfigure $c -encoding utf-8
+              return $c
 	    }
 	    file|write {
 		# record a move on close
@@ -282,6 +284,7 @@ namespace eval engine {
 
 		set t [mktemp]
 		set c [::open $t w]
+                fconfigure $c -encoding utf-8
 		dict set moc $c [list $t [file join $outdir $dst]]
 
 		template message "generating $dst in $t"

commit d43607e9517bd705f4bbfcf1b605f22feb49bc82

Fix a typo introduced in commit:cd37430

engine/tcl/engine.tcl
@@ -272,7 +272,7 @@ namespace eval engine {
 	    }
 	    file|read {
               set dst [file join [dotgen template dir] $dst]
-              message "reading $dst"
+              template message "reading $dst"
               set c [::open $dst r]
               fconfigure $c -encoding utf-8
               return $c